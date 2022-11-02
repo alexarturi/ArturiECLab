cf6440b151b44bf0d7f064d8af846388905989b8
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-11-02 12:11:38 -0400

    11/2

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    public Calculator(){
+    }
+
+    public double roundedToHundredth(double toRound){
+        return Math.round(toRound*100)/100.0;
+    }//round method
+
+    public double add(double a, double b){
+        return roundedToHundredth(a+b);
+    }
+
+    public double subtract(double a, double b){
+        return roundedToHundredth(a-b);
+    }
+
+    public double multiply(double a, double b){
+        return roundedToHundredth(a*b);
+    }
+
+    public double divide(double a, double b){
+        return roundedToHundredth(a/b);
+    }
+
+    public double mod(double a, double b){
+        return roundedToHundredth(a%b);
+    }
+
+    public double exponent(double a, double power){
+        return roundedToHundredth(Math.pow(a, power));
+    }
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+import java.util.Scanner;
+
+public class CalculatorRunner {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        Calculator test = new Calculator();
+        System.out.print("Enter an expression with *, /, +, -, %, or ^: ");
+        String initial = scan.nextLine();
+        int ind = initial.indexOf(" ");
+        double first = Double.parseDouble(initial.substring(0,ind));
+        String operation = initial.substring(ind+1, ind+2);
+        double second = Double.parseDouble(initial.substring(ind+3));
+        System.out.println();
+
+        if (operation.equals("+")){
+            System.out.println(test.add(first, second));
+        } else if (operation.equals("-")){
+            System.out.println(test.subtract(first, second));
+        } else if (operation.equals("*")){
+            System.out.println(test.multiply(first,second));
+        } else if (operation.equals("/")){
+            System.out.println(test.divide(first,second));
+        } else if (operation.equals("%")){
+            System.out.println(test.mod(first,second));
+        } else {
+            System.out.println(test.exponent(first, second));
+        }
+    }
 }
